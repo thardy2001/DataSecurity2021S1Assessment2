ac41a403e51715dd977eeeab78d5c76d9b9cdca6
Lochie <lochiewestfall@gmail.com>
2021-04-20 21:43:23 +1000

    Rename files, Add tests for each handwritten table, Add separate lochie a2 file

--- ListCreator.py
+++ ListCreator.py
@@ -1,16 +1,26 @@
+import pyperclip
+
 file = open("input.txt", "r")
 
 lines = file.readlines()
 
 array = []
 
+flatten = True
+
 for line in lines:
     str_list = line.strip().replace("\t", " ").split(" ")
 
     int_list = list(map(int, str_list))
 
-    array.append(int_list)
+    if not flatten:
+        array.append(int_list)
+    else:
+        for i in int_list:
+            array.append(i)
 
+pyperclip.copy(str(array))
+print("copied to clipboard")
 print(array)
 
 """
