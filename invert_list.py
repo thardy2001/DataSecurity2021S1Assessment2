8caa73339e6039d99408823ef462319d238977af
Lochie <lochiewestfall@gmail.com>
2021-05-05 01:14:26 +1000

    Modify inverse expansion

--- invert_list.py
+++ invert_list.py
@@ -3,10 +3,10 @@ import numpy as np
 
 expansion = np.array([32, 1, 2, 3, 4, 5, 4, 5, 6, 7, 8, 9, 8, 9, 10, 11, 12, 13, 12, 13, 14, 15, 16, 17, 16, 17, 18, 19, 20, 21, 20, 21, 22, 23, 24, 25, 24, 25, 26, 27, 28, 29, 28, 29, 30, 31, 32, 1])
 
-inverse = np.zeros(32) - 1
+inverse = np.zeros(32)
 
 for i in range(len(expansion)):
-    if inverse[expansion[i]-1] < 0:
+    if not i%6 == 0 and not i%6==5:
         inverse[expansion[i]-1] = i + 1
 
 out = "["
