04beefdb481377dc5047350532bc1c4ba1de9d98
Lochie <lochiewestfall@gmail.com>
2021-05-06 22:12:40 +1000

    Add output files and decryption input and output

--- a2_lochie.py
+++ a2_lochie.py
@@ -4,6 +4,8 @@ import time
 initial = [58, 50, 42, 34, 26, 18, 10, 2, 60, 52, 44, 36, 28, 20, 12, 4, 62, 54, 46, 38, 30, 22, 14, 6, 64, 56, 48, 40, 32, 24, 16, 8, 57, 49, 41, 33, 25, 17, 9, 1, 59, 51, 43, 35, 27, 19, 11, 3, 61, 53, 45, 37, 29, 21, 13, 5, 63, 55, 47, 39, 31, 23, 15, 7]
 final = [40, 8, 48, 16, 56, 24, 64, 32, 39, 7, 47, 15, 55, 23, 63, 31, 38, 6, 46, 14, 54, 22, 62, 30, 37, 5, 45, 13, 53, 21, 61, 29, 36, 4, 44, 12, 52, 20, 60, 28, 35, 3, 43, 11, 51, 19, 59, 27, 34, 2, 42, 10, 50, 18, 58, 26, 33, 1, 41, 9, 49, 17, 57, 25]
 expansion = [32, 1, 2, 3, 4, 5, 4, 5, 6, 7, 8, 9, 8, 9, 10, 11, 12, 13, 12, 13, 14, 15, 16, 17, 16, 17, 18, 19, 20, 21, 20, 21, 22, 23, 24, 25, 24, 25, 26, 27, 28, 29, 28, 29, 30, 31, 32, 1]
+# generated expansion_inv using a separate script by looping through expansion and swapping the index and values
+# the values in the left and right columns were chosen to be dropped (i%6==0 or i%6==5)
 expansion_inv = [2, 3, 4, 5, 8, 9, 10, 11, 14, 15, 16, 17, 20, 21, 22, 23, 26, 27, 28, 29, 32, 33, 34, 35, 38, 39, 40, 41, 44, 45, 46, 47]
 straight = [16, 7, 20, 21, 29, 12, 28, 17, 1, 15, 23, 26, 5, 18, 31, 10, 2, 8, 24, 14, 32, 27, 3, 9, 19, 13, 30, 6, 22, 11, 4, 25]
 choice1_left = [57, 49, 41, 33, 25, 17, 9, 1, 58, 50, 42, 34, 26, 18, 10, 2, 59, 51, 43, 35, 27, 19, 11, 3, 60, 52, 44, 36]
@@ -140,7 +142,7 @@ def des(plaintext, key, variant=0, rounds=16, encrypt=True, log_level=0):
         left = right
         right = round_end
 
-        """ This order might be wrong. Not 100% sure. Won't affect avalanche results but might get marked down """
+        # save for future functions
         round_texts.append(left + right)
 
         if log_level > 0:
@@ -155,7 +157,7 @@ def des(plaintext, key, variant=0, rounds=16, encrypt=True, log_level=0):
 
 
 def average(array):
-    # return an array of the averages of the ith element in each array
+    # return a 1d array where element i is the average of the i'th element in each array in the 2d input
     avg = []
     for i in range(len(array[0])):
         total = 0
@@ -238,36 +240,58 @@ def columnar(strings):
 
 
 # read plaintext and key
-file = open("input.txt", "r")
-plaintext = file.readline().rstrip("\n")
-key = file.readline().rstrip("\n")
-file.close()
+input = open("input_encrypt.txt", "r")
+plaintext = input.readline().rstrip("\n")
+key = input.readline().rstrip("\n")
+input.close()
 
-print("ENCRYPTION")
+output = open("output_encrypt_avalanche.txt", "w")
+
+print("ENCRYPTION", file=output)
 
 start_time = time.perf_counter()
 # encrypt the plaintext using the key with DES0
 ciphertext, rounds = des(plaintext=plaintext, key=key, variant=0)
 stop_time = time.perf_counter()
 
-print("Plaintext P:  ", plaintext)
-print("Key K:        ", key)
-print("Ciphertext C: ", ciphertext)
-print("Running time: ", stop_time - start_time, "seconds")
+print("Plaintext P:  ", plaintext, file=output)
+print("Key K:        ", key, file=output)
+print("Ciphertext C: ", ciphertext, file=output)
+print("Running time: ", stop_time - start_time, "seconds", file=output)
 
-print("\nAvalanche:")
+print("\nAvalanche:", file=output)
 
 # generate the plaintext and key permutations
 plain_perms, key_perms = generate_permutations(plaintext, key)
 # perform all avalanche tests
 diffs = [full_avalanche(plain_perms, key_perms, i) for i in range(4)]
 
-print("\nP and Pi under K")
-print(columnar(["Round", "DES0", "DES1", "DES2", "DES3"]))
+print("\nP and Pi under K", file=output)
+print(columnar(["Round", "DES0", "DES1", "DES2", "DES3"]), file=output)
 for i in range(len(diffs[0][0])):
-    print(columnar([i, round(diffs[0][0][i]), round(diffs[1][0][i]), round(diffs[2][0][i]), round(diffs[3][0][i])]))
+    print(columnar([i, round(diffs[0][0][i]), round(diffs[1][0][i]), round(diffs[2][0][i]), round(diffs[3][0][i])]), file=output)
 
-print("\nP under K and Ki")
-print(columnar(["Round", "DES0", "DES1", "DES2", "DES3"]))
+print("\nP under K and Ki", file=output)
+print(columnar(["Round", "DES0", "DES1", "DES2", "DES3"]), file=output)
 for i in range(len(diffs[0][1])):
-    print(columnar([i, round(diffs[0][1][i]), round(diffs[1][1][i]), round(diffs[2][1][i]), round(diffs[3][1][i])]))
+    print(columnar([i, round(diffs[0][1][i]), round(diffs[1][1][i]), round(diffs[2][1][i]), round(diffs[3][1][i])]), file=output)
+
+output.close()
+
+# read plaintext and key
+input = open("input_decrypt.txt", "r")
+in_decrypt = input.readline().rstrip("\n")
+key = input.readline().rstrip("\n")
+input.close()
+
+output = open("output_decrypt.txt", "w")
+
+print("DECRYPTION", file=output)
+
+decrypt, d_rounds = des(plaintext=in_decrypt, key=key, variant=0, encrypt=False)
+
+print("Ciphertext C: ", ciphertext, file=output)
+print("Key K:        ", key, file=output)
+print("Plaintext P:  ", decrypt, file=output)
+
+output.close()
